7a57aeb821e85fae323550019952a4eeba35bf3e
cstuser <cstuser@10.1.3.78>
2015-05-06 20:41:55 -0400

    eehh

--- IP_Project/src/ip_project/experiments/calculus/NewSportsBikeContainer.java
+++ IP_Project/src/ip_project/experiments/calculus/NewSportsBikeContainer.java
@@ -22,7 +22,8 @@ public class NewSportsBikeContainer extends MIContainer implements Resources{
 	Slider mSlider1, mSlider2;
 	TranslateTransition anim1, anim2;
 	ImageView image;
-	
+	LineChart<Number, Number> mGraph1, mGraph2;
+
 	
 	final BarChart<String,Number> barGraph;
 	
@@ -33,35 +34,101 @@ public class NewSportsBikeContainer extends MIContainer implements Resources{
 	public NewSportsBikeContainer(){
 		
 		
-		String Ystr1 = "$$$";
 		
-		Axis<String> xAxis = new CategoryAxis();
-		NumberAxis yAxis =new NumberAxis(Ystr1,-5,	5, 1);
+		// example of slider set up
+		mSlider1 = new Slider(0, 1, 0.75);
+		mSlider1.setShowTickMarks(true);
+		mSlider1.setShowTickLabels(true);
+		mSlider1.setMajorTickUnit(0.1);
+		mSlider1.setMinorTickCount(0);
+		mSlider1.setSnapToTicks(true);
+		mSlider1.setId("Slider 1");
+		this.addInputs(mSlider1);
+
+		mSlider2 = new Slider(3, 10, 5);
+		mSlider2.setShowTickMarks(true);
+		mSlider2.setShowTickLabels(true);
+		mSlider2.setMajorTickUnit(1);
+		mSlider2.setMinorTickCount(0);
+		mSlider2.setSnapToTicks(true);
+		mSlider2.setId("Slider 2");
+		this.addInputs(mSlider2);
 		
 		
 		
-		barGraph =  new BarChart<String,Number>(xAxis,yAxis);
+		
+		String Ystr1 = "Units Sold";
+		String Ystr2 = "Profit";
+			
+
+		String Xstr = "Cost";
 		
 		
-		XYChart.Series series1 = new XYChart.Series();
+		
+		NumberAxis xAxis1 = new NumberAxis(Xstr,-5,	5, 1);
+		NumberAxis yAxis1 =new NumberAxis(Ystr1,-5,	5, 1);
+		
+		NumberAxis xAxis2 = new NumberAxis(Xstr,-5,	5, 1);
+		NumberAxis yAxis2 =new NumberAxis(Ystr2,-5,	5, 1);
+		
+		
+		
+		
+		
+		mGraph1 = new LineChart<Number, Number>(xAxis1, yAxis1);
+		mGraph2 = new LineChart<Number, Number>(xAxis2, yAxis2);
+
+		
+		
+		//barGraph =  new BarChart<String,Number>(xAxis,yAxis);
+		
+		
+		/*XYChart.Series series1 = new XYChart.Series();
         series1.setName("2003");       
         series1.getData().add(new XYChart.Data(25601.34, "austria"));
         series1.getData().add(new XYChart.Data(20148.82, "brazil"));
         series1.getData().add(new XYChart.Data(10000, "france"));
         series1.getData().add(new XYChart.Data(35407.15, "italy"));
         series1.getData().add(new XYChart.Data(12000, "usa"));    
-		
+		*/
 		
         
         
 		
-		this.addGraphs(barGraph);
+		this.addGraphs(mGraph1);
+		this.addGraphs(mGraph2);
+
+        
+        
+        
+        
+        
 		
 		
 	}
 	
 	
 	
+	public double calculateUnitSales(double price) {	
+		return  70000  - 200 * price;	
+	}
+	
+	
+	public double calculateSales(double price) {	
+		return (this.calculateUnitSales(price)) * price;	
+	}
+	
+	
+	public double calculateTotalCost (double price){
+		
+		return mSlider1.getValue() 
+	}
+	
+	public double calculateProfit() {
+		
+		return ()
+		
+	}
 	
 	
 	
